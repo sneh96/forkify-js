268bc8ad97f4c2c9be92bd2aa558ba4f03457dd9
Patel <snehalpatel7095@gmail.com>
2021-02-15 18:09:30 -0500

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'b60b2447-91ba-4548-82ac-8ab4249e6785';
+export const MODAL_CLOSE_SEC = 1.5;
